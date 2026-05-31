470406c524b0c402bca0f16db7277eaadc336135
AbdelrahmanSamirz <shekaba@gmail.com>
2026-05-31 17:01:46 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoAsp.Net8_Session01_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
