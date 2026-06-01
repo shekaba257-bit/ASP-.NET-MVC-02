d1b5f9c8cd7988e76844f03003632e6a011d6d24
AbdelrahmanSamirz <shekaba@gmail.com>
2026-06-01 23:37:50 +0300

    Demo

---
+++ Controllers/PlanController.cs
@@ -0,0 +1,41 @@
+﻿using DemoAsp.Net8_Session01_.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAsp.Net8_Session01_.Controllers
+{
+    public class PlanController : Controller
+    {
+        //[1] DataBase Connection
+        private readonly GymDbContext Context;
+
+        public PlanController()
+        {
+            Context = new GymDbContext();
+        }
+        //[2] Get :: URL/Plan/Index
+
+
+        public async Task<IActionResult> Index()
+        {  
+            //Async => As Treat With Database 
+            var Plans =await Context.Plans.ToListAsync();
+            return View(Plans);
+        }
+
+        //[3] Get :: BaseURL/Plan/Detais/{id}
+                                      //Detection ==> ([From Route] int id)
+        public async Task<IActionResult> Details(int id)
+        {
+            //FindAsync ==> If Data is  Cashed  Will Return it if not Return From Database
+            var plans = await Context.Plans.FindAsync(id);  
+            if(plans == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plans); 
+           
+        }
+
+    }
+}
---
+++ FluentConfigration/PlanConfigration.cs
@@ -0,0 +1,36 @@
+﻿using DemoAsp.Net8_Session01_.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DemoAsp.Net8_Session01_.FluentConfigration
+{
+    public class PlanConfigration : IEntityTypeConfiguration<Plan>
+    {
+        public void Configure(EntityTypeBuilder<Plan> builder)
+        {
+            builder.Property(P => P.Name)
+               .HasColumnType("Varchar")
+               .HasMaxLength(30);
+
+            builder.Property(P => P.Description)
+                .HasMaxLength(200);
+
+
+            builder.Property(P => P.Price)
+                .HasPrecision(10, 2); //decimal(10,2)
+
+
+            builder.Property(P => P.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+            //Check Constrant => Must on Table
+            //Duration Must Be (1-365)Days
+            builder.ToTable<Plan>(TB =>
+
+            {
+                TB.HasCheckConstraint("PlanDurationCheck", "DurationDays Between 1 and 365");
+
+            });
+        }
+    }
+}
